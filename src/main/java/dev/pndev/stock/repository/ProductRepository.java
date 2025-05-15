45530bc6a9367c2df4cc1c4ba064a7b48ce8858c
Priscila Nascimento <pnmelo2808@gmail.com>
2025-05-15 02:31:29 -0300

    Feat: Repository created

--- src/main/java/dev/pndev/stock/repository/ProductRepository.java
+++ src/main/java/dev/pndev/stock/repository/ProductRepository.java
@@ -1,20 +1,20 @@
 package dev.pndev.stock.repository;
 
-import dev.pndev.stock.model.Product;
+import dev.pndev.stock.model.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ProductRepository extends JpaRepository <Product, Long>{
+public interface ProductRepository extends JpaRepository <ProductEntity, Long>{
 
-    boolean existsProductById(Long id);
 
-    boolean productReplenishById(Long id);
+    List<ProductEntity> findAllByQuantityLessThan(int threshold);
 
-    Product findProductById(Long id);
-
-    List<Product> findAllByReplenishTrue();
+    @Query("SELECT CASE WHEN p.quantity < 5 THEN true ELSE false END FROM ProductEntity p WHERE p.id = :id")
+    boolean isProductReplenishById(@Param("id") Long id);
 
 }
